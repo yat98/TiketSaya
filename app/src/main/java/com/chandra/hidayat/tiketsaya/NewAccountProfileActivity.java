cfa97455d59c59527a823d7d182960019677fc22
yat98 <hidayatchandra08@gmail.com>
2019-03-14 00:28:47 +0800

    Add Success Sign Up Activity

--- app/src/main/java/com/chandra/hidayat/tiketsaya/NewAccountProfileActivity.java
+++ app/src/main/java/com/chandra/hidayat/tiketsaya/NewAccountProfileActivity.java
@@ -1,5 +1,6 @@
 package com.chandra.hidayat.tiketsaya;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.ImageButton;
 
 public class NewAccountProfileActivity extends AppCompatActivity {
     private ImageButton btnBack;
+    private Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +17,20 @@ public class NewAccountProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_account_profile);
 
         btnBack = findViewById(R.id.btn_back);
+        btnContinue = findViewById(R.id.btn_continue);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
+            }
+        });
+
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSuccessSignUp = new Intent(NewAccountProfileActivity.this, SuccessSignUpActivity.class);
+                startActivity(intentSuccessSignUp);
             }
         });
     }
---
+++ app/src/main/java/com/chandra/hidayat/tiketsaya/SuccessSignUpActivity.java
@@ -0,0 +1,13 @@
+package com.chandra.hidayat.tiketsaya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuccessSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_sign_up);
+    }
+}
